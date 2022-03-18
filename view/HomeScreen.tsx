d2886e19e0371fd84a570bc1e7d1cd22c15bd01a
Kevin Oliveira <kevinklgvg@gmail.com>
2022-03-18 16:48:09 -0300

    ok naviagtion app

--- view/HomeScreen.tsx
+++ view/HomeScreen.tsx
@@ -8,19 +8,19 @@ const HomeScreen = ({ navigation }:any) => {
     return (
       <View style={styles.container}>
         <Text>Dashboard</Text>
-        <br/>
+
         <Text>Exercicios de PAM (programação e algoritmo mobile)</Text>
-        <br/>
+
         <Button
           title="Km to Metros"
           onPress={() => navigation.navigate('KmToMetros')}
         />
-        <br/>
+
         <Button
           title="Km/H to Milhas/H"
           onPress={() => navigation.navigate('KmhToMilhash')}
         />
-        <br/>
+
         <Button
           title="Real to Dolar $$$"
           onPress={() => navigation.navigate('RealToDolar')}
--- view/KmhToMilhash.tsx
+++ view/KmhToMilhash.tsx
@@ -1,11 +1,17 @@
 
-import { Text, View, Button } from 'react-native';
+import { SetStateAction, useState } from 'react';
+import { Text, View, Button, TextInput  } from 'react-native';
 import { styles } from '../styles/main';
 
 const KmhToMilhash = () => {
+
+  const [km, setKm] = useState(0);
+
     return (
       <View style={styles.container}>
         <Text>Km/h To Milhas/h Screen</Text>
+        <TextInput onChange={(e)=>{setKm(e.target.value)}} />
+        <Text>{ km.toString }</Text>
       </View>
     );
 }
